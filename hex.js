a52f64f86770d141c6399c46387081896b99da22
Aristocrat4 <galadanelia1@gmail.com>
2021-11-24 20:19:09 +0400

    edit a little bit of code

--- hex.js
+++ hex.js
@@ -1,11 +1,13 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-let bgCol = "";
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+let bgCol = "#";
 btn.addEventListener('click', function() {
     for ( let i = 0; i <6; i++){
     const randomNumber = Math.trunc(Math.random()*hex.length);
-    bgCol += `${String(hex[randomNumber])}`;
+    bgCol += hex[randomNumber];
 }
-document.querySelector('.color').textContent = `#${bgCol}`;
-document.body.style.backgroundColor = `#${bgCol}`;
-bgCol = "";
+document.querySelector('.color').textContent = `${bgCol}`;
+document.body.style.backgroundColor = `${bgCol}`;
+bgCol = "#";
 });
